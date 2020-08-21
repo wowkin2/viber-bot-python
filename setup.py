a9f9c68d7fcb85f46476d22eaa5e66fdb033a70d
wowkin2 <djwowkin@gmail.com>
2020-08-21 17:04:01 +0300

    Fixed issue with version variable

--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 from setuptools import setup
 
-# exec(open('viberbot/version.py').read())  # TODO: remove if not needed anymore
-from viberbot.version import __version__
+exec(open('viberbot/version.py').read())
 
 
 setup(
     name='viberbot',
     version=__version__,
-    packages=['viberbot', 'viberbot.api', 'viberbot.api.viber_requests',
-              'viberbot.api.messages', 'viberbot.api.messages.data_types'],
+    packages=[
+        'viberbot',
+        'viberbot.api',
+        'viberbot.api.viber_requests',
+        'viberbot.api.messages',
+        'viberbot.api.messages.data_types',
+    ],
     install_requires=['future', 'requests'],
     url='https://github.com/Viber/viber-bot-python',
 )
